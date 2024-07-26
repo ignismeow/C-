e5c13b78d887fee2724306c913600bb6d42f38af
ignismeow <134957807+ignismeow@users.noreply.github.com>
2024-07-26 19:45:51 +0500

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,20 @@
+//this is my first c++ program
+/*I don't wann 
+
+spoil my time*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	string myName = "Saad"; 
+	int myAge = 55;
+    double mycgpa = 24.4;
+    char myletter = 'A';
+    bool isgendermale = true;
+
+	//cout << "\"Hello Saad!\"" <<endl;
+    //cout << "How is your day today" <<endl;
+    cout <<"My name is "<<myName<<". My age is "<<myAge<<". My cgpa is "<< mycgpa <<". My favourite charcter is "<< myletter << ". My gender is "<<isgendermale<<"."<< endl;
+    
+    return 0;
+};
